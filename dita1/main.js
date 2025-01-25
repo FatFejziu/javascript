f601f3663967276738a85a9e08066c505a82e3bb
FatFejziu <fat.fejziu@gmail.com>
2025-01-25 14:38:59 +0100

    day2

---
+++ dita1/main.js
@@ -0,0 +1,72 @@
+var a = 50+50;
+
+console.log(a)
+console.log(50+50)
+
+var b = 20;
+var c = 30;
+var d = b + c;
+console.log(d)
+
+
+var e = 40;
+var f = 10;
+var g= e - f;
+console.log(g)
+
+var h = 100;
+var i = 5;
+var j = h / i;
+console.log(j)
+
+var k = 10;
+var l = 4;
+var m = k % l;
+console.log(m)
+
+var n = 99;
+// n=n+1;
+n++;
+console.log(n)
+
+var o =101;
+o--;
+console.log(o)
+
+var p =20;
+var q = p ** 2;
+console.log(q);
+
+
+
+
+
+var x = 15;
+x+=5;
+console.log(x)
+
+var r = 35;
+r-=5;
+console.log(r)
+
+var s = 10;
+s*=7
+console.log(s)
+
+var t = 200;
+t/=2
+console.log(t)
+
+var u = 10;
+u%=5
+console.log(u)
+//  a o barazim
+var v = 5;
+var w = 5;
+console.log(v == w);
+// a jane te ndyshushme
+var x =6;
+var y =7
+console.log(x != y);
+
+
